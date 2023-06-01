6af3cf7c7794f88780f75a1faddff83818fb27ad
Alex Grant <alex@Alexs-MacBook-Pro.local>
2023-06-01 17:52:41 +0930

    stripped out analog sensor handler

--- td2arduino.ino
+++ td2arduino.ino
@@ -1,15 +1,3 @@
-
-// Number of Analog sensor inputs
-const int NUM_SENSORS = 6;
-const unsigned long reportingInterval = 1000; // millisecond
-const unsigned long readingInterval = 100; // millisecond
-
-int sensors[NUM_SENSORS] = {A0, A1, A2, A3, A4, A5};
-unsigned long sensorReadings = 0;
-unsigned long sensorValues[NUM_SENSORS];
-unsigned long nextReport;
-unsigned long nextRead;
-
 // Define the number of digital pins
 const int NUM_PINS = 5;
 
@@ -33,39 +21,9 @@ void setup() {
     pinMode(pins[i], OUTPUT);
   }
 
-  for (int i=0; i < NUM_SENSORS; i++) {
-    sensorValues[i]=0;
-  }
-
-  nextReport = millis() + reportingInterval;
-  nextRead = millis() + readingInterval;
 }
 
 void loop() {
-
-  // Analog sensor handler
-  unsigned long now = millis();
-
-  if (now > nextRead) {
-    for (int i=0; i<NUM_SENSORS; i++) {
-     sensorValues[i] += analogRead(sensors[i]);
-   }
-    sensorReadings++;
-    nextRead = millis() + readingInterval;
-  }
-
-  if (now > nextReport) {
-    for (int i =0; i<NUM_SENSORS; i++){
-      Serial.print(sensorValues[i]/sensorReadings);
-      Serial.print(" ");
-      sensorValues[i] = 0;
-    }
-    Serial.print("\n");
-
-    sensorReadings = 0;
-    nextReport = millis() + reportingInterval;
-  }
-
 
   // Button illumination handler
   if (Serial.available()) {
